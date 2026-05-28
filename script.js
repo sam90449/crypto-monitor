9cc0b6863592cc22b9cac71f5b2a58fb943c8c16
sam90449 <jacky123cheung@gmail.com>
2026-05-28 08:25:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,27 +2,20 @@ async function loadCoin(side) {
 
     const symbol =
         document
-        .getElementById(
-            side + "-input"
-        )
+        .getElementById(side + "-input")
         .value
         .trim()
         .toUpperCase();
 
     const resultBox =
-        document.getElementById(
-            side + "-result"
-        );
+        document.getElementById(side + "-result");
 
-    resultBox.innerHTML =
-        "Loading...";
+    resultBox.innerHTML = "Loading...";
 
     try {
 
         const response =
-            await fetch(
-                `/api/coin/${symbol}`
-            );
+            await fetch("/api/macro");
 
         const data =
             await response.json();
@@ -35,192 +28,89 @@ async function loadCoin(side) {
             return;
         }
 
+        let coinPrice = "N/A";
+
+        if (symbol === "BTC") {
+
+            coinPrice = data.btc;
+
+        } else if (symbol === "ETH") {
+
+            coinPrice = data.eth;
+
+        } else {
+
+            coinPrice = "UNSUPPORTED";
+        }
+
         resultBox.innerHTML = `
 
 <div class="coin-title">
-${data.symbol}USDT
+${symbol}/USDT
 </div>
 
 <div class="coin-price">
 PRICE:
 <br>
-${Number(data.price).toFixed(4)}
+${coinPrice}
 </div>
 
 <br>
 
-TARGET:
+<div class="coin-info">
+24H CHANGE:
 <br>
-${Number(data.target).toFixed(4)}
+${data.btcChange}
+</div>
 
-<br><br>
-
-5 MA:
 <br>
-${Number(data.ma5).toFixed(4)}
 
-<br><br>
-
-15 MA:
+<div class="coin-info">
+VOLUME:
 <br>
-${Number(data.ma15).toFixed(4)}
+${data.btcVolume}
+</div>
 
-<br><br>
-
-30 MA:
 <br>
-${Number(data.ma30).toFixed(4)}
 
-<br><br>
-
-5 MA SLOPE:
+<div class="coin-info">
+MARKET STATUS:
 <br>
-${Number(data.ma5slope).toFixed(4)}%
+${data.icon} ${data.status}
+</div>
 
-<br><br>
-
-15 MA SLOPE:
 <br>
-${Number(data.ma15slope).toFixed(4)}%
 
-<br><br>
-
-30 MA SLOPE:
+<div class="coin-info">
+FEAR INDEX:
 <br>
-${Number(data.ma30slope).toFixed(4)}%
+${data.fear}
+(${data.fearText})
+</div>
 
-<br><br>
-
-FINAL PREDICT:
 <br>
-${Number(data.predict).toFixed(4)}%
 
-<br><br>
-
+<div class="coin-info">
 UPDATE:
 <br>
 ${data.updateTime}
+</div>
 
 `;
 
     } catch (e) {
 
         resultBox.innerHTML =
-            "LOAD FAIL";
+            "LOAD FAILED<br><br>" + e;
     }
 }
-
-document
-.getElementById("left-load")
-.onclick = () =>
-loadCoin("left");
-
-document
-.getElementById("right-load")
-.onclick = () =>
-loadCoin("right");
 
 loadCoin("left");
 loadCoin("right");
 
-async function loadMacro() {
+setInterval(() => {
 
-    try {
+    loadCoin("left");
+    loadCoin("right");
 
-        const response =
-            await fetch(
-                "/api/macro"
-            );
-
-        const data =
-            await response.json();
-
-        const box =
-            document.getElementById(
-                "macro-box"
-            );
-
-        box.innerHTML = `
-
-<h2>
-BTC 宏觀方向（4-24H）
-</h2>
-
-<div style="
-font-size:42px;
-margin-top:10px;
-">
-${data.icon}
-</div>
-
-<div style="
-font-size:36px;
-font-weight:bold;
-margin-top:10px;
-">
-${data.status}
-</div>
-
-<br>
-
-BTC:
-${data.btc}
-
-<br><br>
-
-ETH:
-${data.eth}
-
-<br><br>
-
-BTC 24H CHANGE:
-${data.btcChange}
-
-<br><br>
-
-BTC VOLUME:
-${data.btcVolume}
-
-<br><br>
-
-TOTAL CAP:
-${data.totalCap}
-
-<br><br>
-
-BTC DOM:
-${data.btcDom}
-
-<br><br>
-
-FEAR:
-${data.fear}
-(
-${data.fearText}
-)
-
-<br><br>
-
-HK UPDATE:
-${data.updateTime}
-
-`;
-    }
-
-    catch(e){
-
-        document
-        .getElementById(
-            "macro-box"
-        )
-        .innerHTML =
-            "MACRO LOAD FAIL";
-    }
-}
-
-loadMacro();
-
-setInterval(
-    loadMacro,
-    60000
-);
+}, 30000);
